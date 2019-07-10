0264222075d5fa0dee6d8a11a30a88130064beed
Pozas91 <pozas91@gmail.com>
2019-07-10 19:51:21 +0200

    Lists & State

--- src/App.js
+++ src/App.js
@@ -36,6 +36,15 @@ class App extends Component {
         })
     };
 
+    deletePersonHandler = (personIndex) => {
+        const persons = this.state.persons;
+
+        // Remove person
+        persons.splice(personIndex, 1);
+        
+        this.setState({persons: persons});
+    };
+
     togglePersonsHandler = () => {
         const doesShow = this.state.showPersons;
         this.setState({showPersons: !doesShow});
@@ -48,8 +57,9 @@ class App extends Component {
         if (this.state.showPersons) {
             persons = (
                 <div>
-                    {this.state.persons.map(person => {
-                        return <Person name={person.name} age={person.age}/>
+                    {this.state.persons.map((person, index) => {
+                        return <Person name={person.name} age={person.age}
+                                       click={() => this.deletePersonHandler(index)}/>
                     })}
                 </div>
             );
