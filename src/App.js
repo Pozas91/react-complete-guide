391ddf393143c2b73c1976eeccfbcc972348fd5f
Pozas91 <pozas91@gmail.com>
2019-07-10 19:35:14 +0200

    Handling Dynamic Content "The JavaScript Way"

--- src/App.js
+++ src/App.js
@@ -42,26 +42,33 @@ class App extends Component {
     };
 
     render() {
+
+        let persons = null;
+
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    <Person
+                        name={this.state.persons[0].name}
+                        age={this.state.persons[0].age}/>
+                    <Person
+                        name={this.state.persons[1].name}
+                        age={this.state.persons[1].age}
+                        click={this.switchNameHandler.bind(this, 'Max!')}
+                        changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+                    <Person
+                        name={this.state.persons[2].name}
+                        age={this.state.persons[2].age}/>
+                </div>
+            );
+        }
+
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
                 <p>This is really working!</p>
                 <button onClick={this.togglePersonsHandler}>Switch Name</button>
-                {this.state.showPersons === true ?
-                    <div>
-                        <Person
-                            name={this.state.persons[0].name}
-                            age={this.state.persons[0].age}/>
-                        <Person
-                            name={this.state.persons[1].name}
-                            age={this.state.persons[1].age}
-                            click={this.switchNameHandler.bind(this, 'Max!')}
-                            changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
-                        <Person
-                            name={this.state.persons[2].name}
-                            age={this.state.persons[2].age}/>
-                    </div> : null
-                }
+                {persons}
             </div>
         )
     }
