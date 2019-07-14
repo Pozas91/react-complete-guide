683725368aad02920a619876d96994ce4d6434dc
Pozas91 <pozas91@gmail.com>
2019-07-14 22:45:17 +0200

    Enabling & Using CSS Modules (Optionally, but doesn't work).
    This part is necesary in all versions of react-scripts, in current version doesn't necessary.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
-import './App.css';
-import Radium, {StyleRoot} from 'radium';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -70,11 +69,7 @@ class App extends Component {
             font: 'inherit',
             border: '1px solid blue',
             padding: '8px',
-            cursor: 'pointer',
-            ':hover': {
-                backgroundColor: 'lightgreen',
-                color: 'black'
-            }
+            cursor: 'pointer'
         };
 
         let persons = null;
@@ -94,33 +89,27 @@ class App extends Component {
             );
 
             style.backgroundColor = 'red';
-            style[':hover'] = {
-                backgroundColor: 'salmon',
-                color: 'white'
-            }
         }
 
-        let classes = [];
+        const assignedClasses = [];
 
         if (this.state.persons.length <= 2) {
-            classes.push('red'); // classes = ['red']
+            assignedClasses.push(classes.red); // classes = ['red']
         }
 
         if (this.state.persons.length <= 1) {
-            classes.push('bold'); // classes = ['red', 'bold']
+            assignedClasses.push(classes.bold); // classes = ['red', 'bold']
         }
 
         return (
-            <StyleRoot>
-                <div className="App">
-                    <h1>Hi, I'm a React App</h1>
-                    <p className={classes.join(' ')}>This is really working!</p>
-                    <button style={style} onClick={this.togglePersonsHandler}>Switch Name</button>
-                    {persons}
-                </div>
-            </StyleRoot>
+            <div className={classes.App}>
+                <h1>Hi, I'm a React App</h1>
+                <p className={assignedClasses.join(' ')}>This is really working!</p>
+                <button style={style} onClick={this.togglePersonsHandler}>Switch Name</button>
+                {persons}
+            </div>
         )
     }
 }
 
-export default Radium(App);
+export default App;
