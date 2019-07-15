fec9c9fec741e9b9f700941cd4c30af7b95dbf41
Pozas91 <pozas91@gmail.com>
2019-07-15 20:31:42 +0200

    Enablig & Using CSS Modules Adding Pseudo Selectors Working with Media Queries

--- src/App.js
+++ src/App.js
@@ -64,15 +64,8 @@ class App extends Component {
 
         console.info('Render method!');
 
-        const style = {
-            backgroundColor: 'white',
-            font: 'inherit',
-            border: '1px solid blue',
-            padding: '8px',
-            cursor: 'pointer'
-        };
-
         let persons = null;
+        let btnClass = '';
 
         if (this.state.showPersons) {
             persons = (
@@ -88,7 +81,7 @@ class App extends Component {
                 </div>
             );
 
-            style.backgroundColor = 'red';
+            btnClass = classes.Red;
         }
 
         const assignedClasses = [];
@@ -105,7 +98,7 @@ class App extends Component {
             <div className={classes.App}>
                 <h1>Hi, I'm a React App</h1>
                 <p className={assignedClasses.join(' ')}>This is really working!</p>
-                <button style={style} onClick={this.togglePersonsHandler}>Switch Name</button>
+                <button className={btnClass} onClick={this.togglePersonsHandler}>Switch Name</button>
                 {persons}
             </div>
         )
